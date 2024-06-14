c287438260f39a656189a891a73f011a6c2b816d
Raghav Khandelwal <raghavk2806@gmail.com>
2024-06-14 15:51:05 +0530

    Forgot Password added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-import { registerUser, loginUser } from "../services/userService.js";
+import { registerUser, loginUser, sendEmail } from "../services/userService.js";
 import { ApiError } from "../errors/ApiError.js";
 
 export async function register (req,res,next) {
@@ -13,6 +13,15 @@ export async function register (req,res,next) {
 export async function login (req,res,next) {
     try {
         const user = await loginUser(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        next(ApiError.badRequest(err.message));
+    }
+};
+
+export async function mail (req,res,next) {
+    try {
+        const user = await sendEmail(req.body);
         res.status(201).json(user);
     } catch (err) {
         next(ApiError.badRequest(err.message));
--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,17 @@
 import { findUserByEmail, createUser } from "../repositories/userRepository.js";
 import bcrypt from "bcrypt";
+import nodemailer from "nodemailer";
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.APP_PASSWORD,
+  }
+})
 
 export async function registerUser(userData) {
   try {
@@ -8,7 +20,7 @@ export async function registerUser(userData) {
       throw new Error("User already exists");
     } else {
       await createUser(userData);
-      return userData;
+      return {user: userData.userName};
     }
   } catch (err) {
     throw new Error("Unable to register!");
@@ -39,5 +51,23 @@ export async function loginUser(userData) {
     }
   } catch (err) {
     throw new Error("Unable to Login!");
+  }
+}
+
+export async function sendEmail(userData) {
+  try {
+    const info = await transporter.sendMail({
+      from: {
+        name: "Rudhira",
+        address: process.env.USER
+      },
+      to: userData.userName, // list of receivers
+      subject: "Reset your password", // Subject line
+      text: "Reset your password here", // plain text body
+      html: "<a href='https://www.youtube.com/'>Reset your password</a>", // html body
+    })
+    return {user: userData};
+  } catch (err) {
+    throw new Error("Cannot send mail")
   }
 }
